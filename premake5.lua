c684e8182a8f356cf8cbe9efaa90f93070ad9ccc
chase sunstrom <casunstrom+github@gmail.com>
2024-11-11 11:27:35 -0600

    commit

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+workspace "MinecraftClone"
+    architecture "x64"
+    configurations { "Debug", "Release" }
+    startproject "MinecraftClone"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "MinecraftClone"
+    location "MinecraftClone"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "MinecraftClone/src/**.h",
+        "MinecraftClone/src/**.c",
+        "MinecraftClone/src/**.cpp",
+        "MinecraftClone/src/**.hpp"
+    }
+
+    includedirs {
+        "include",
+    }
+
+    libdirs {
+        "lib"
+    }
+
+    links {
+        "opengl32.lib",
+        "lib/glfw3.lib",
+        "lib/glew32s.lib"
+    }
+
+    filter "configurations:Debug"
+    defines { "DEBUG", "_ITERATOR_DEBUG_LEVEL=2", "GLEW_STATIC" }
+    runtime "Debug"
+    symbols "on"
+        
+filter "configurations:Release"
+    defines { "NDEBUG", "_ITERATOR_DEBUG_LEVEL=0", "GLEW_STATIC" }
+    runtime "Release"
+    optimize "on"
